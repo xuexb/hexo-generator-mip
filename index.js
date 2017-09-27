9fa1436fd908d78066f5259a4e96e3dd4ee8b5f9
xuexb <fe.xiaowu@gmail.com>
2017-09-27 14:59:16 +0800

    Pass by config params

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var getConfig = require('./lib/config');
 var config = getConfig(hexo);
 
 if (config.enable) {
-    require('./lib/style.js')(hexo);
-    require('./lib/fix-a-href.js')(hexo);
-    require('./lib/a.js')(hexo);
-    require('./lib/img.js')(hexo);
-    require('./lib/canonical.js')(hexo);
+    require('./lib/style.js')(hexo, config);
+    require('./lib/fix-a-href.js')(hexo, config);
+    require('./lib/a.js')(hexo, config);
+    require('./lib/img.js')(hexo, config);
+    require('./lib/canonical.js')(hexo, config);
 }
