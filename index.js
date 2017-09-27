5e44148b4b173ea6747ee474923d8f9203754e87
xuexb <fe.xiaowu@gmail.com>
2017-09-27 20:54:27 +0800

    Update config

--- index.js
+++ index.js
@@ -9,12 +9,14 @@
 
 var extend = require('extend');
 var defaults = require('./config');
-var config = extend({}, defaults, hexo.theme.config.mip, hexo.config.mip);
 
-if (config.enable) {
-    require('./lib/style.js')(hexo, config);
-    require('./lib/fix-a-href.js')(hexo, config);
-    require('./lib/a.js')(hexo, config);
-    require('./lib/img.js')(hexo, config);
-    require('./lib/canonical.js')(hexo, config);
-}
+hexo.extend.generator.register('mip', function (locals) {
+    var config = extend({}, defaults, hexo.theme.config.mip, hexo.config.mip);
+    if (config.enable) {
+        require('./lib/style.js')(hexo, config);
+        require('./lib/fix-a-href.js')(hexo, config);
+        require('./lib/a.js')(hexo, config);
+        require('./lib/img.js')(hexo, config);
+        require('./lib/canonical.js')(hexo, config);
+    }
+});
