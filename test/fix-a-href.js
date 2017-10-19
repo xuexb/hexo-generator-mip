91e6c91a396fba9ed42f96dfbc3b375994169af6
xuexb <fe.xiaowu@gmail.com>
2017-10-19 09:37:27 +0800

    feat: update fix-a.href test case

--- test/fix-a-href.js
+++ test/fix-a-href.js
@@ -6,10 +6,80 @@
 var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
+var extend = require('extend');
 var exec = require('../lib/fix-a-href');
 var expect = chai.expect;
 
 chai.use(sinonChai);
 
 describe('fix-a-href.js', function () {
+    var spy;
+    var callback;
+
+    /**
+     * 注册 filter
+     *
+     * @param  {Ojbect | undefined} data 注入数据
+     * @param {Object | undefined} config 注入配置数据
+     */
+    var register = function (data, config) {
+        var hexo = {};
+        hexo.extend = {};
+        hexo.extend.filter = {};
+        hexo.extend.filter.register = function () {};
+
+        extend(hexo, data);
+
+        spy = sinon.spy(hexo.extend.filter, 'register');
+        exec(hexo, config || {});
+        callback = spy.getCall(0).args[1];
+    };
+
+    it('register filter', function () {
+        register();
+
+        expect(spy).to.have.been.calledOnce;
+        expect(spy).to.have.been.calledWith('after_render:html');
+    });
+
+    it('callback function', function () {
+        register();
+        expect(callback).to.be.a('function');
+    });
+
+    it('append href', function () {
+        register();
+
+        var result = callback([
+            '<a href="#">1</a>',
+            '<A>1</A>',
+            '<a',
+            '<div>',
+                '<a>2</a>',
+                '<a  href="#">2</a>',
+                '<a id="id">2</a>',
+                '<a id="id"  href="#">2</a>',
+                '<a    class="class" name="test">2</a>',
+                '<a  href="#"  class="class" name="test">2</a>',
+                '<aid="test"></a>',
+                '<a   href="test"></a>',
+            '</div>'
+        ].join(''));
+
+        expect(result).to.equal([
+            '<a href="#">1</a>',
+            '<A href="#">1</A>',
+            '<a',
+            '<div>',
+                '<a href="#">2</a>',
+                '<a  href="#">2</a>',
+                '<a href="#" id="id">2</a>',
+                '<a id="id"  href="#">2</a>',
+                '<a href="#"    class="class" name="test">2</a>',
+                '<a  href="#"  class="class" name="test">2</a>',
+                '<aid="test"></a>',
+                '<a   href="test"></a>',
+            '</div>'
+        ].join(''));
+    });
 });
