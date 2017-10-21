ed33d7e57bbcb768128c4a823c7347000791b583
xuexb <fe.xiaowu@gmail.com>
2017-10-21 15:06:59 +0800

    test: update style test case

--- test/style.js
+++ test/style.js
@@ -3,19 +3,49 @@
  * @author xuexb <fe.xiaowu@gmail.com>
  */
 
+var fs = require('fs');
+var path = require('path');
 var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
 var extend = require('extend');
+var del = require('del');
+var mkdirp = require('mkdirp');
 var exec = require('../lib/style');
 var expect = chai.expect;
 
 chai.use(sinonChai);
 
+/* eslint-disable fecs-camelcase */
+
 describe('style.js', function () {
     var filter;
     var helper;
     var callback;
+    var mockBase = path.resolve(__dirname, 'mock');
+    var styleFilePath = path.resolve(mockBase, 'source/css');
+    var HTML_TEMPLATE = [
+        '<!doctype html>',
+        '<html>',
+            '<head>',
+                '<title>test</title>',
+            '</head>',
+            '<body>',
+                '<h1>test</h1>',
+            '</body>',
+        '</html>'
+    ].join('');
+
+    /**
+     * 写入测试数据
+     *
+     * @param  {string} content  内容
+     * @param  {string | undefined} filename 文件名
+     */
+    var mock = function (content, filename) {
+        filename = path.resolve(styleFilePath, filename || Date.now() + Math.round(Math.random() * 1000) + '.css');
+        fs.writeFileSync(filename, content);
+    };
 
     /**
      * 注册 filter
@@ -40,7 +70,12 @@ describe('style.js', function () {
     };
 
     beforeEach(function () {
+        mkdirp.sync(styleFilePath);
         register();
+    });
+
+    afterEach(function () {
+        del.sync(mockBase);
     });
 
     it('register filter', function () {
@@ -66,6 +101,207 @@ describe('style.js', function () {
             '</head>'
         ].join('');
 
-        expect(head).to.equal(head);
+        expect(callback(head)).to.equal(head);
+    });
+
+    describe('the default file is loaded under `config.theme_dir`', function () {
+        it('css files should be loaded successfully', function () {
+            mock('html {width: 0;}');
+            mock('body {height: 0;}');
+
+            register({
+                theme_dir: mockBase
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.be.a('string');
+            expect(result).to.have.string('<style mip-custom>');
+            expect(result).to.have.string('html {width: 0;}');
+            expect(result).to.have.string('body {height: 0;}');
+        });
+
+        it('empty css folders should not be processed', function () {
+            register({
+                theme_dir: mockBase
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.be.a('string').and.not.have.string('<style mip-custom>');
+        });
+
+        it('should ignore the _ at the beginning of the file', function () {
+            mock('html {width: 0;}', '_html.css');
+            mock('body {height: 0;}');
+
+            register({
+                theme_dir: mockBase
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.have.string('<style mip-custom>');
+            expect(result).to.have.string('body {height: 0;}').and.not.have.string('html {width: 0;}');
+        });
+    });
+
+    describe('there are `mip.css` in the configuration file', function () {
+        it('`mip.css` is a string', function () {
+            mock('html {width: 0;}', 'html.css');
+
+            register({
+                theme_dir: mockBase
+            }, {
+                css: 'html.css'
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.have.string('<style mip-custom>').and.have.string('html {width: 0;}');
+        });
+
+        it('`mip.css` is a empty string', function () {
+            register({
+                theme_dir: mockBase
+            }, {
+                css: ''
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.not.have.string('<style mip-custom>');
+        });
+
+        it('`mip.css` is a array', function () {
+            mock('html {width: 0;}', 'html.css');
+
+            register({
+                theme_dir: mockBase
+            }, {
+                css: ['html.css']
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.have.string('<style mip-custom>').and.have.string('html {width: 0;}');
+        });
+
+        it('`mip.css` is a empty array', function () {
+            register({
+                theme_dir: mockBase
+            }, {
+                css: []
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.not.have.string('<style mip-custom>');
+        });
+
+        it('`mip.css` is a non-existent file', function () {
+            register({
+                theme_dir: mockBase
+            }, {
+                css: 'error.css'
+            });
+
+            expect(function () {
+                callback(HTML_TEMPLATE);
+            }).to.throw('error.css');
+        });
+
+        it('`mip.css` is a empty file', function () {
+            mock('', 'html.css');
+
+            register({
+                theme_dir: mockBase
+            }, {
+                css: 'html.css'
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.not.have.string('<style mip-custom>');
+        });
+    });
+
+    describe('there are `mip.cssmin` in the configuration file', function () {
+        it('`mip.cssmin` is a true', function () {
+            mock('body {width:0px;} ');
+
+            register({
+                theme_dir: mockBase
+            }, {
+                cssmin: true
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.have.string('<style mip-custom>').and.have.string('body{width:0}');
+        });
+
+        it('`mip.cssmin` is a false', function () {
+            mock('body {width:0px;} ');
+
+            register({
+                theme_dir: mockBase
+            }, {
+                cssmin: false
+            });
+
+            var result = callback(HTML_TEMPLATE);
+
+            expect(result).to.have.string('<style mip-custom>').and.have.string('body {width:0px;} ');
+        });
+    });
+
+    describe('separate the `<noscript>` tag in `<head>`', function () {
+        it('Inserted before `<noscript>`', function () {
+            mock('body {width:0px;} ');
+
+            register({
+                theme_dir: mockBase
+            });
+
+            var result = callback([
+                '<!doctype html>',
+                '<html>',
+                    '<head>',
+                        '<title>test</title>',
+                        '<noscript>test</noscript>',
+                    '</head>',
+                    '<body>',
+                        '<h1>test</h1>',
+                    '</body>',
+                '</html>'
+            ].join('\n'));
+
+            expect(result).to.have.string('<style mip-custom>').and.have.string('</style><noscript>');
+        });
+
+        it('`<noscript>` in body', function () {
+            mock('body {width:0px;} ');
+
+            register({
+                theme_dir: mockBase
+            });
+
+            var result = callback([
+                '<!doctype html>',
+                '<html>',
+                    '<head>',
+                        '<title>test</title>',
+                    '</head>',
+                    '<body>',
+                        '<noscript>test</noscript>',
+                        '<h1>test</h1>',
+                    '</body>',
+                '</html>'
+            ].join('\n'));
+
+            expect(result).to.have.string('<style mip-custom>').and.not.have.string('</style><noscript>');
+        });
     });
 });
+/* eslint-enable fecs-camelcase */
